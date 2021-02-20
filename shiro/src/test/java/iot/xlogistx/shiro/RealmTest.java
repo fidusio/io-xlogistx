c5aa307d5cf2a53ea59aafe335464ff2d06b76f6
javaconsigliere <javaconsigliere@gmail.com>
2021-02-20 14:34:39 -0800

    Added valid and invalid login test

--- shiro/src/test/java/iot/xlogistx/shiro/RealmTest.java
+++ shiro/src/test/java/iot/xlogistx/shiro/RealmTest.java
@@ -5,6 +5,7 @@ import io.xlogistx.shiro.XlogistXShiroRealm;
 import io.xlogistx.shiro.authc.DomainUsernamePasswordToken;
 import org.apache.shiro.SecurityUtils;
 
+import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.env.BasicIniEnvironment;
 import org.apache.shiro.env.Environment;
 import org.apache.shiro.mgt.SecurityManager;
@@ -15,11 +16,28 @@ import org.zoxweb.shared.security.SubjectIDDAO;
 import org.zoxweb.shared.security.shiro.ShiroRealmStore;
 import org.zoxweb.shared.util.BaseSubjectID;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class RealmTest {
+
+
+
     @BeforeAll
     public static void loadRealm()
     {
+        Environment env = new BasicIniEnvironment("classpath:shiro-xlog.ini");
+        SecurityManager securityManager = env.getSecurityManager();
+        System.out.println(securityManager.getClass().getName());
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        SubjectIDDAO subjectIDDAO = new SubjectIDDAO();
+        subjectIDDAO.setSubjectType(BaseSubjectID.SubjectType.USER);
+        subjectIDDAO.setSubjectID("root");
+        XlogistXShiroRealm realm = ShiroUtil.getRealm(null);
+        ShiroRealmStore srs = realm.getShiroRealmStore();
+        srs.addSubject(subjectIDDAO);
+        srs.setSubjectPassword("root", "secret");
     }
 
 
@@ -34,40 +52,28 @@ public class RealmTest {
     {
 
     }
+    
 
     @Test
-    public void testIniLoading()
+    public void testValidLogin()
     {
-//        Environment env = new BasicIniEnvironment("classpath:shiro.ini");
-//        SecurityManager securityManager = env.getSecurityManager();
-//        SecurityUtils.setSecurityManager(securityManager);
-//        Subject subject = SecurityUtils.getSubject();
-//        UsernamePasswordToken token =  new UsernamePasswordToken("root", "secret");
-//        subject.login(token);
-//        System.out.println(subject.getPrincipal());
-    }
-
-    @Test
-    public void testXlogIniLoading()
-    {
-        Environment env = new BasicIniEnvironment("classpath:shiro-xlog.ini");
-        SecurityManager securityManager = env.getSecurityManager();
-        System.out.println(securityManager.getClass().getName());
-        SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
-        SubjectIDDAO subjectIDDAO = new SubjectIDDAO();
-        subjectIDDAO.setSubjectType(BaseSubjectID.SubjectType.USER);
-        subjectIDDAO.setSubjectID("root");
-        XlogistXShiroRealm realm = ShiroUtil.getRealm(null);
-
-
-        ShiroRealmStore srs = realm.getShiroRealmStore();//ResourceManager.SINGLETON.lookup(ResourceManager.Resource.REALM_STORE);
-        srs.setSubjectPassword("root", "secret");
-
-        srs.addSubject(subjectIDDAO);
         DomainUsernamePasswordToken token =  new DomainUsernamePasswordToken("root", "secret", false, null, null);
         subject.login(token);
         System.out.println(subject.getPrincipal());
+        subject.logout();
+    }
+
+    @Test
+    public void testInvalidLogin()
+    {
+        Subject subject = SecurityUtils.getSubject();
+        DomainUsernamePasswordToken token =  new DomainUsernamePasswordToken("root", "secret1", false, null, null);
+        assertThrows(IncorrectCredentialsException.class, ()->{
+            subject.login(token);
+        });
+        System.out.println(subject.getPrincipal());
+        subject.logout();
     }
 
 
