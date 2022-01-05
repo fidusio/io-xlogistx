512612e74f133c333ae04ce58b2eabd2981bcb27
mnael <javaconsigliere@gmail.com>
2022-01-04 16:37:56 -0800

    update

--- security/src/main/java/io/xlogistx/ssl/SSLSessionCallback.java
+++ security/src/main/java/io/xlogistx/ssl/SSLSessionCallback.java
@@ -4,16 +4,11 @@ package io.xlogistx.ssl;
 
 
 import io.xlogistx.common.net.BaseSessionCallback;
-
 import java.io.OutputStream;
+
 public abstract class SSLSessionCallback extends BaseSessionCallback<SSLSessionConfig>
 {
-    @Override
-    public void exception(Exception e) {
-        // exception handling
 
-        log.info( e + "");
-    }
     final public OutputStream get()
     {
         return getConfig().sslos;
