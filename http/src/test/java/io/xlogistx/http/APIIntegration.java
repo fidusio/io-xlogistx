6d43c42e0e8249e4e254cf819c023a8653257ea8
javaconsigliere <javaconsigliere@gmail.com>
2023-09-13 17:43:50 -0700

    Code update

--- http/src/test/java/io/xlogistx/http/APIIntegration.java
+++ http/src/test/java/io/xlogistx/http/APIIntegration.java
@@ -1,16 +1,16 @@
 package io.xlogistx.http;
 
-import org.zoxweb.server.http.HTTPCall;
+import org.zoxweb.server.http.HTTPAPIEndPoint;
+import org.zoxweb.server.http.HTTPUtil;
+import org.zoxweb.server.task.TaskUtil;
 import org.zoxweb.server.util.GSONUtil;
-import org.zoxweb.shared.http.HTTPAuthorization;
-import org.zoxweb.shared.http.HTTPMediaType;
-import org.zoxweb.shared.http.HTTPMessageConfig;
-import org.zoxweb.shared.http.HTTPMessageConfigInterface;
-import org.zoxweb.shared.util.GetValue;
-import org.zoxweb.shared.util.NVGenericMap;
-import org.zoxweb.shared.util.ParamUtil;
+import org.zoxweb.shared.http.*;
+import org.zoxweb.shared.util.*;
 
-public class APIIntegration {
+import java.util.ArrayList;
+
+public class APIIntegration
+{
 
     public static void main(String ...args)
     {
@@ -26,49 +26,92 @@ public class APIIntegration {
             String httpMethod = params.stringValue("request");
 
 
-            HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(apiURL, apiURI, httpMethod);
-            hmci.setAccept(HTTPMediaType.APPLICATION_JSON);
-            hmci.setContentType(HTTPMediaType.APPLICATION_JSON);
-            hmci.setURLEncodingEnabled(false);
-            hmci.getHeaders().add("revision", "2023-07-15");
-            hmci.setAuthorization(new HTTPAuthorization(apiKeyID, apiKey));
+            HTTPMessageConfigInterface config = HTTPMessageConfig.createAndInit(apiURL, apiURI, httpMethod);
+            config.setAccept(HTTPMediaType.APPLICATION_JSON);
+            config.setContentType(HTTPMediaType.APPLICATION_JSON);
+            config.setURLEncodingEnabled(false);
+            config.getHeaders().add("revision", "2023-07-15");
+            config.setAuthorization(new HTTPAuthorization(apiKeyID, apiKey));
 
-            NVGenericMap content = new NVGenericMap();
-            NVGenericMap data = new NVGenericMap("data");
-            content.add(data);
-            NVGenericMap attributes = new NVGenericMap("attributes");
-            data.add("type", "profile");
-            data.add(attributes);
-            attributes.add(params.asNVPair("email"));
-            attributes.add(params.asNVPair("first_name"));
+
+
+
+
+
+
+            BiDataEncoder<HTTPMessageConfigInterface, NVPairList, HTTPMessageConfigInterface> encoder = new BiDataEncoder<HTTPMessageConfigInterface, NVPairList, HTTPMessageConfigInterface>() {
+                @Override
+                public HTTPMessageConfigInterface encode(HTTPMessageConfigInterface hmci, NVPairList nvPairList) {
+                    NVGenericMap content = new NVGenericMap();
+                    NVGenericMap data = new NVGenericMap("data");
+                    content.add(data);
+                    NVGenericMap attributes = new NVGenericMap("attributes");
+                    data.add("type", "profile");
+                    data.add(attributes);
+
+                    for (NVPair nvp : nvPairList.values()) {
+                        attributes.add(nvp);
+                    }
+
+                    hmci.setContent(GSONUtil.toJSONDefault(content));
+
+
+                    return hmci;
+                }
+
+            };
+
+            NVPairList parameters = new NVPairList("Paramaters", new ArrayList<>());
+
+            parameters.add(params.asNVPair("email"));
+            parameters.add(params.asNVPair("first_name"));
 
             if (params.nameExists("last_name"))
-                attributes.add(params.asNVPair("last_name"));
-
-            String contentJson = GSONUtil.toJSONDefault(content, true);
+                parameters.add(params.asNVPair("last_name"));
 
 
-            String  json = GSONUtil.toJSONDefault(hmci, true);
-            System.out.println(json);
-            hmci = GSONUtil.fromJSONDefault(json, HTTPMessageConfig.class, true);
-            System.out.println(GSONUtil.toJSONDefault(content, true));
-            GetValue<?> rev = hmci.getHeaders().get("revision");
-            System.out.println(rev);
+            HTTPAPIEndPoint<NVPairList, NVGenericMap> userAPI = new HTTPAPIEndPoint<NVPairList, NVGenericMap>(config)
+                    .setName("UserCollection")
+                    .setDescription("Sending user info to klaviyo")
+                    .setDataEncoder(encoder)
+                    .setDataDecoder(HTTPUtil.NVGM_DECODER)
+                    .setRateController(new RateController("klaviyo", "75/min"))
+                    .setScheduler(TaskUtil.getDefaultTaskScheduler());
+
+            HTTPCallBack<NVPairList, NVGenericMap> callback = new HTTPCallBack<NVPairList, NVGenericMap>(parameters) {
+                @Override
+                public void accept(HTTPAPIResult<NVGenericMap> apiResult)
+                {
+                    System.out.println(apiResult.getData());
+                }
+            };
+
+
+
+            //userAPI.asyncCall(callback);
+            System.out.println(userAPI.syncCall(parameters));
+
+
+
+
+
+
+
             //hmci.getHeaders().add("revision", "2023-07-15");
 
-            System.out.println(contentJson);
 
-            hmci.setContent(GSONUtil.toJSONDefault(content));
 
-            System.out.println(hmci.getAuthorization().toHTTPHeader());
-            System.out.println(HTTPCall.send(hmci));
+//            System.out.println(config.getAuthorization().toHTTPHeader());
+//            System.out.println(HTTPCall.send(config));
+
+
 
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
+        TaskUtil.waitIfBusyThenClose(50);
 
     }
 }
