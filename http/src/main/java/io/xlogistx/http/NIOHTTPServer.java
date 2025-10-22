5b67eec0b4f932452f7eba90393f7b154a467012
javaconsigliere <javaconsigliere@gmail.com>
2025-10-22 11:05:12 -0700

    bumped the version to 1.4.6

--- http/src/main/java/io/xlogistx/http/NIOHTTPServer.java
+++ http/src/main/java/io/xlogistx/http/NIOHTTPServer.java
@@ -55,7 +55,7 @@ import static org.zoxweb.server.net.ssl.SSLContextInfo.Param.PROTOCOLS;
 
 public class NIOHTTPServer
         implements DaemonController, GetNamedVersion {
-    public static final String VERSION = "1.4.5";
+    public static final String VERSION = "1.4.6";
 
     public final static LogWrapper logger = new LogWrapper(NIOHTTPServer.class).setEnabled(false);
     private final HTTPServerConfig config;
